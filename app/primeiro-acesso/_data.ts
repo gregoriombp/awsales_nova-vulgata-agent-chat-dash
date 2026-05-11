ea10d00d6aaef26229dc66e0a9497936431bba5d
Gregorio Pinheiro <gregpinheiro@outlook.com>
2026-05-11 15:37:17 -0300

    Add mock org data for primeiro-acesso onboarding

---
+++ app/primeiro-acesso/_data.ts
@@ -0,0 +1,16 @@
+export const ONBOARDING_ORG = {
+  name: "Magalu Pay",
+  razaoSocial: "MAGAZINE LUIZA S.A.",
+  cnpj: "47.960.950/0001-21",
+  plan: "Enterprise",
+  contractTerm: "12 meses",
+  valorImplementacao: "R$ 12.000,00",
+  valorMensal: "R$ 4.800,00",
+  accountManager: "Lucas Vieira",
+}
+
+export const ONBOARDING_USER = {
+  name: "Ricardo Almeida",
+  firstName: "Ricardo",
+  email: "ricardo.almeida@magalupay.com.br",
+}
